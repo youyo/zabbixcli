f9e32d7865db10be1221365e6c7ee9a63d0e4e7e
youyo <1003ni2@gmail.com>
2016-10-06 18:27:32 +0900

    first commit

---
+++ commands.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"github.com/youyo/zabbixctl/command"
+)
+
+var GlobalFlags = []cli.Flag{}
+
+var Commands = []cli.Command{
+	{
+		Name:   "create",
+		Usage:  "",
+		Action: command.CmdCreate,
+		Flags:  []cli.Flag{},
+	},
+	{
+		Name:   "delete",
+		Usage:  "",
+		Action: command.CmdDelete,
+		Flags:  []cli.Flag{},
+	},
+}
+
+func CommandNotFound(c *cli.Context, command string) {
+	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
+	os.Exit(2)
+}
